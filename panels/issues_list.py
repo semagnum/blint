706aa6ca55889604d7d10cc735cd44e5d7bd91f9
Spencer Magnusson <semagnum@gmail.com>
2023-07-25 12:14:50 -0600

    #4 filter by fixable and error severity

--- panels/issues_list.py
+++ panels/issues_list.py
@@ -21,16 +21,35 @@ from ..model import LintIssue, get_sort_value
 class BT_UL_Issues(bpy.types.UIList):
     """Blender UI List to display issues. Includes filtering and sorting."""
 
+    is_fixable: bpy.props.BoolProperty(
+        name="Fixable Only",
+        description="Only show issues with automated fixes",
+        default=False
+    )
+    """Toggle to only show issues with automated fixes."""
+    is_error: bpy.props.BoolProperty(
+        name="Errors Only",
+        description="Only show issues categorized as errors",
+        default=False
+    )
+    """Toggle to only show issues categorized as errors."""
+
     def draw_item(self, context, layout, data, issue: LintIssue, icon, active_data, active_propname,
                   index):
         issue.draw(layout)
+
+    def draw_filter(self, context, layout):
+        row = layout.row()
+        row.prop(self, 'filter_name', text='')
+        row.prop(self, 'is_fixable', text='Fixable', toggle=1)
+        row.prop(self, 'is_error', text='Error only', icon='ERROR')
 
     def filter_items(self, context, data, propname):
         issues = getattr(data, propname)
         helper_funcs = bpy.types.UI_UL_list
 
         # Default return values.
-        flt_flags = []
+        flt_flags = [self.bitflag_filter_item] * len(issues)
         flt_neworder = []
 
         # Filtering by name
@@ -38,8 +57,12 @@ class BT_UL_Issues(bpy.types.UIList):
             flt_flags = helper_funcs.filter_items_by_name(self.filter_name, self.bitflag_filter_item,
                                                           issues, 'description')
 
-        if not flt_flags:
-            flt_flags = [self.bitflag_filter_item] * len(issues)
+        for idx in range(len(flt_flags)):
+            issue: LintIssue = issues[idx]
+            if ((self.is_fixable and len(issue.fix_expr) == 0)
+                    or (self.is_error and issue.severity_icon != 'ERROR')
+            ):
+                flt_flags[idx] = 0
 
         _sort = [(idx, (get_sort_value(issue), getattr(issue, 'description', '').lower()))
                  for idx, issue in enumerate(issues)]
